d9d72867f72fa8e491cdb8b203c28104f9b2a63c
Phill Pham <innifine@gmail.com>
2025-04-26 21:57:09 -0400

    fixed skater and goalie data scraping

--- nhl_player_goalie_scraper.R
+++ nhl_player_goalie_scraper.R
@@ -1,42 +1,88 @@
 # data scraper
 library(tidyverse)
 library(rvest)
-
+source("get_standings.R")
 # Importing data ----------------------------------------------------------
 
 print("scraping goalies")
-goalies = rvest::read_html(x = "https://www.hockey-reference.com/playoffs/NHL_2025_goalies.html") %>% 
-  html_elements("table") %>%  
-  html_table(header = T) %>% 
-  pluck(1) %>%
-  (\(tbl) {
-    colnames(tbl) <- make.names(tbl[1, ])
-    tbl %>% slice(2:nrow(tbl))
-  })()
-goalies$date_retrieved = Sys.Date()
+page_goalies = read_html("https://www.hockey-reference.com/playoffs/NHL_2025_goalies.html")
+table_nodes = page_goalies %>%  html_node("table#stats")
+goalies = table_nodes %>% 
+  html_table(fill=TRUE, header = T)
+colnames(goalies) = goalies[1,]
+goalies = goalies %>% filter(Rk != "Rk")
+goalies = goalies %>% 
+  dplyr::mutate(date_retrieved = Sys.Date()) %>% 
+  dplyr::rename(rank = Rk,
+                team = Tm,
+                games_played = GP,
+                games_started = GS,
+                wins = W,
+                losses = L,
+                goals_against = GA,
+                shots_against = SA,
+                saves = SV,
+                saves_pct = `SV%`,
+                goals_against_avg = GAA,
+                shutouts = SO,
+                goalie_point_shares = GPS,
+                mins_played = MIN,
+                quality_starts = QS,
+                quality_starts_rate = `QS%`,
+                really_bad_starts = RBS,
+                goals_against_avgleague = `GA%-`,
+                goals_saved_above_avg = GSAA,
+                goals = G,
+                assists = A,
+                points = PTS,
+                penality_mins = PIM)
 
 print("scraping players")
-players = rvest::read_html(x = "https://www.hockey-reference.com/playoffs/NHL_2025_skaters.html") %>% 
-  html_elements("table") %>%
-  html_table() %>% 
-  pluck(1) %>%
-  (\(tbl) {
-    colnames(tbl) <- make.names(tbl[1, ], unique = TRUE)
-    tbl %>% slice(2:nrow(tbl))
-  })()
-players$date_retrieved = Sys.Date()
+page_skaters = read_html("https://www.hockey-reference.com/playoffs/NHL_2025_skaters.html")
+table_nodes = page_skaters %>%  html_node("table#stats")
+skaters = table_nodes %>% 
+  html_table(fill=TRUE, header = T)
+colnames(skaters) = skaters[1,]
+skaters = repair_names(skaters)
+skaters = skaters %>% filter(Rk != "Rk")
+skaters = skaters %>%
+  dplyr::mutate(date_retrieved = Sys.Date()) %>%
+  dplyr::rename(rank = Rk,
+                team = Tm,
+                position = Pos,
+                goals = G,
+                assists = A,
+                points = PTS,
+                plus_minus = `+/-`,
+                penality_mins = PIM,
+                even_strength_goals = EV,
+                power_play_goals = PP,
+                short_handed_goals = SH,
+                game_winning_goals = GW,
+                even_strength_assists = EV1,
+                power_play_assists = PP1,
+                short_handed_assists = SH1,
+                shots_on_goal = S,
+                shooting_percentage = `S%`,
+                total_ice_time = TOI,
+                avg_total_ice_time = ATOI,
+                blocks = BLK,
+                hits = HIT,
+                faceoff_wins = FOW,
+                faceoff_losses = FOL,
+                faceoff_pct = `FO%`)
 
-if(!dir.exists('data')){
-  dir.create('data')
-} else {
-  print("skipping, directory already exists")
-}
-
+# fetch standings
+print("Fetching team standings")
+standings = get_standings()
 
 print("writing data")
 
 fname_goalie = str_glue("data/goalies_{Sys.Date()}.csv")
 readr::write_csv(x = goalies, file = fname_goalie)
 
-fname_player = str_glue("data/players_{Sys.Date()}.csv")
-readr::write_csv(x = goalies, file = fname_player)
+fname_skaters = str_glue("data/skaters_{Sys.Date()}.csv")
+readr::write_csv(x = skaters, file = fname_skaters)
+
+fname_standings = str_glue("data/standings_{Sys.Date()}.csv")
+readr::write_csv(x = standings, file = fname_standings)
